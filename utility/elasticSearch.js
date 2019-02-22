38be366800965f0a26b1a276866b04381cfea6cf
laxmikant <laxmikant@CORP.AMAZONWORKSPACES.COM>
2019-02-22 17:43:02 +0530

    bulk insert pipeline

--- utility/elasticSearch.js
+++ utility/elasticSearch.js
@@ -1,11 +1,6 @@
 var elasticsearch = require('elasticsearch');
-var clientlocal = new elasticsearch.Client({
-   hosts: [ 'http://localhost:9200']
-});
-
 var client = new elasticsearch.Client({
-   hosts: [ 'https://vpc-fs-searchtool-djvapr2kgvdzhckjo3mzlilqbm.us-west-2.es.amazonaws.com'],
-   timeout: 12000000
+   hosts: [ 'http://localhost:9200']
 });
 
 module.exports=client;
